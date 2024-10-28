6f760e507e29de777133cb8e1e37b0fc37fcc6a0
Baqari-Fruidze <baqarifruidze@gmail.com>
2024-10-28 20:13:22 +0400

    credentials

--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -1,5 +1,41 @@
 import { NextAuthOptions } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
 
 export const options: NextAuthOptions = {
-  authorize,
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        username: {
+          label: "Username:",
+          type: "text",
+          placeholder: "enter username",
+        },
+        password: {
+          label: "Password:",
+          type: "text",
+          placeholder: "enter password",
+        },
+      },
+      async authorize(credentials) {
+        console.log(credentials);
+        const user = {
+          id: "1",
+          name: "beqa",
+          password: "beqa123",
+          email: "beqa@gmail.com",
+          age: 21,
+        };
+
+        if (
+          credentials?.username == user.name &&
+          credentials?.password == user.password
+        ) {
+          return user;
+        } else {
+          return null;
+        }
+      },
+    }),
+  ],
 };
